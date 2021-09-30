99c1efb74b1d9c358a752a1360aabfd8214a7a7f
hung <truongphuochung@gmail.com>
2021-10-01 00:54:33 +0900

    revised version

--- js/script.js
+++ js/script.js
@@ -37,4 +37,21 @@ $(document).ready(function() {
         autoplay:true,
         autoplayTimeout:3000
     })
+
+    $('#slider3 .owl-carousel').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        },
+    })
 });
